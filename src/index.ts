5dd0c10a6b78bb7e368b4bfedc0292c9cff64982
NoeliaGaray <103612855+NoeliaGaray@users.noreply.github.com>
2022-06-04 19:16:01 -0300

    termine

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 let cantidad: number = 270;
+let mayores: number = 0;
+let menores: number = 0; 
 let personasEnDisco: number[] = new Array(cantidad);
 let indice: number;
 
@@ -8,32 +10,16 @@ function Aleatorio(menorValor: number, mayorValor: number): number {
 
 for (indice = 0; indice <= cantidad; indice++) {
   personasEnDisco[indice] = Aleatorio(19, 40);
-}
-
-for (indice = 0; indice <= cantidad; indice++) {
   console.log(personasEnDisco[indice]);
 }
-
 console.log(`En la disco hay ${indice - 1} personas`);
 
-// if (personasEnDisco[indice] >= personasEnDisco[21]){
-//   console.log(`En la disco hay ${personasEnDisco[indice-1]} personas`);
-// } else {
-//   console.log(`En la disco hay ${personasEnDisco[indice-1]} personas menores de 21`);
-// }
-
-// while (personasEnDisco[indice]>=21) {
-//   console.log(`hay ${indice} mayores de 21`)
-// }
-
-// if (personasEnDisco[indice]<21){
-//   console.log(`En la disco hay ${indice} personas menores de 21`)
-// } else {
-//   console.log(`En la disco hay ${indice} personas mayores de 21`)
-// }
-
-// console.log(`En la disco hay ${indice} personas menores de 21`);
-
-// if (indice<21){
-//   console.log(`Hay ${indice} menores de 21`);
-// }
+for (indice = 0; indice <= cantidad; indice++) {
+if(personasEnDisco[indice]>=21) {
+mayores++;
+} else {
+  menores++;
+}
+}
+console.log(`En la disco hay ${mayores-1} mayores de 21`)
+console.log(`En la disco hay ${menores} menores de 21`)
